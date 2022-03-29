6a9368cdc452ab173fd4f0276fb3a3a2a818dcc8
Milesrat <102177400+Milesrat@users.noreply.github.com>
2022-03-29 19:03:34 +0200

    Add files via upload

---
+++ mqueue.cpp
@@ -0,0 +1,25 @@
+/*
+//class implementing queue in C++
+//the same functionality as STL (Standard Template Library ) class queue in C++ 
+//with usage example
+//Version 1.1
+Consists
+a. Program template
+b. defintion of class mqueue 
+Author: Anna Jeżo  nick: Milesrat
+ */
+ 
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class mqueue {
+ vector<int> elements;
+ int first, last;
+};	
+
+int main() {
+ mqueue	payments;
+	
+ return 0;
+}
